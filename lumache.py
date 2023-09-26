18454e2dee470713f165cf4ea71d45f74a3ded0c
ramajoballester <alvaroramajo1@gmail.com>
2023-09-26 20:47:19 +0200

    Add numpydoc support

--- lumache.py
+++ lumache.py
@@ -21,3 +21,61 @@ def get_random_ingredients(kind=None):
     :rtype: list[str]
     """
     return ["shells", "gorgonzola", "parsley"]
+
+def get_random_recipe(style: str = None, ingredients: list = None):
+    '''
+    Return a random recipe
+
+    Parameters
+    ----------
+    style : str
+        The style of the recipe
+    ingredients : list
+        The ingredients of the recipe
+
+    Returns
+    -------
+    recipe : str
+        The recipe
+
+    Raises
+    ------
+    ValueError
+        If the style is not valid
+
+    Examples
+    --------
+    >>> get_random_recipe()
+    'Spaghetti al pomodoro'
+
+    >>> get_random_recipe(style='spanish')
+    'Paella'
+
+    >>> get_random_recipe(ingredients=['eggs', 'bacon'])
+    'Carbonara'
+
+    >>> get_random_recipe(style='italian', ingredients=['eggs', 'bacon'])
+    'Pastiera'
+    '''
+
+    recipes = {
+        'italian': {
+            'pasta': ['Spaghetti al pomodoro', 'Carbonara', 'Pasta al pesto'],
+            'pizza': ['Margherita', 'Marinara', 'Diavola'],
+            'dessert': ['Tiramisù', 'Pastiera', 'Cannoli']
+        },
+        'spanish': {
+            'Paella': ['Paella']
+        }
+    }
+
+    if style is None and ingredients is None:
+        return 'Spaghetti al pomodoro'
+    
+    if style is not None and style not in recipes.keys():
+        raise ValueError(f'Invalid style {style}')
+    
+    if ingredients is not None:
+        return 'Carbonara'
+    
+    return 'Paella'
