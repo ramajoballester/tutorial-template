d42c130767039db465155d049feb15ebbfbabba5
ramajoballester <alvaroramajo1@gmail.com>
2023-09-28 01:25:37 +0200

    Complete TOC in RTD ?

--- lumache/models/__init__.py
+++ lumache/models/__init__.py
@@ -3,6 +3,6 @@ Docstring for lumache/models/__init__.py
 """
 
 
-from .new_model import NewModel
+from .new_model import NewModel, NewModel2
 
-__all__ = ['NewModel']
+__all__ = ['NewModel', 'NewModel2']
--- lumache/models/new_model.py
+++ lumache/models/new_model.py
@@ -50,3 +50,55 @@ class NewModel:
             _description_, by default None
         """
         pass
+
+
+class NewModel2:
+    """
+    This is a new model
+
+    Parameters
+    ----------
+    new_parameter : str
+        A new parameter
+    
+    Attributes
+    ----------
+    new_parameter : str
+        A new parameter
+    
+    Methods
+    -------
+    new_function(even_newer_parameter: str = None)
+        A new function
+
+    Examples
+    --------
+    >>> new_model = NewModel()
+    >>> new_model.new_parameter
+    'new_parameter'
+    >>> new_model.new_function()
+  
+    """
+
+    def __init__(self, new_parameter: str = None):
+        """ 
+        Class constructor
+
+        Parameters
+        ----------
+        new_parameter : str, optional
+            _description_, by default None
+        """
+
+        self.new_parameter = new_parameter
+
+    def new_function(self, even_newer_parameter: str = None):
+        """
+        A new function
+
+        Parameters
+        ----------
+        even_newer_parameter : str, optional
+            _description_, by default None
+        """
+        pass
